bdc494c56d332e965bd0c97044e837ae4e84e224
Amira Mohamed <49735037+amira-mohamed@users.noreply.github.com>
2019-06-10 00:26:22 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,23 +1,8 @@
-## My name is amira, and the model that I present, as investigated in more depth, 
-## shows how R programming environments work to be optimal.
-
-## In this part, the function 'makeCacheMatrix', builds a set of functions and returns (object) 
-## the functions within a list to the parent environment. 
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
-        }
-        get <- function() x
-        setmean <- function(mean) m <<- mean
-        getmean <- function() m
-        list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
+
 }
 
 
@@ -25,13 +10,5 @@ m <- NULL
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        m <- x$getmean()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
-        data <- x$get()
-        m <- mean(data, ...)
-        x$setmean(m)
-        m
+      
 }
