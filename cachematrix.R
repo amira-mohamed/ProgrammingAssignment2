8143b6fb60372564512503024994cabcf8f1d40b
Amira Mohamed <49735037+amira-mohamed@users.noreply.github.com>
2019-07-02 09:58:53 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,33 @@
+## we are going to write a pair of functions "makeCacheMatrix" and "cacheSolve" which compute the inverse of 
+ ## a invertible matrix. So below is the explanation how thw functions are formed 
+  
+ ## The function "makeCacheMatrix" returns a special "matrix" object that can  
+ ## obtain its inverse for the input which is an invertible square matrix 
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
 }
 
-
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+cachemean <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-      
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
 }
